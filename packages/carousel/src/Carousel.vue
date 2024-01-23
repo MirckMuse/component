1f45316d01055f38a3ba777fe6509958666f1057
Shengjie Zhang4 章圣洁 <mirckzhang@126.com>
2024-01-23 08:37:01 +0800

    组件定义
    1. Carousel

--- packages/carousel/src/Carousel.vue
+++ packages/carousel/src/Carousel.vue
@@ -9,5 +9,27 @@ defineOptions({
   name: "SCarousel"
 });
 
-withDefaults(defineProps<CarouselProps>(), {})
+withDefaults(defineProps<CarouselProps>(), {
+  dots: true,
+  effect: "scrollx",
+  easing: "linear"
+});
+
+defineExpose({
+  goTo: handleGoTo,
+  next: handleNext,
+  prev: handlePrev
+});
+
+function handleGoTo(slideNumber: number, useAnimate = false) {
+  // TODO:
+}
+
+function handleNext() {
+  // TODO:
+}
+
+function handlePrev() {
+  // TODO:
+}
 </script> 
--- packages/carousel/src/typing.ts
+++ packages/carousel/src/typing.ts
@@ -1,5 +1,22 @@
-import type { ComponentSize } from "@stable/shared";
 
 export interface CarouselProps {
-  size?: ComponentSize;
-}   
+  autoplay?: boolean;
+
+  dotPosition?: CarouselDotPosition;
+
+  dots?: boolean;
+
+  dotsClass?: string;
+
+  easing?: string;
+
+  effect?: CarouselEffect;
+
+  afterChange?: (current: number) => void;
+
+  beforeChange?: (from: number, to: number) => void;
+}
+
+export type CarouselDotPosition = 'top' | 'bottom' | 'left' | 'right';
+
+export type CarouselEffect = "scrollx" | "fade";
